c12ddf02265c7656775eb23e0949e516acf64b14
webk0d <webk0d@ya.ru>
2022-01-23 22:21:05 +0300

    Initial Commit

---
+++ HW3.2/HW3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_2App.swift
+//  HW3.2
+//
+//  Created by Сергей Чумаков on 23.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
